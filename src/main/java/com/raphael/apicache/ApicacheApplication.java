35ce769e79a05b18c1908111cb43126822dd7aba
Raphael Willian <raphaelwillian.cardososilva@gmail.com>
2026-03-27 11:44:35 -0300

    feat: Commit Inicial Da Aplicação

---
+++ src/main/java/com/raphael/apicache/ApicacheApplication.java
@@ -0,0 +1,13 @@
+package com.raphael.apicache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicacheApplication.class, args);
+	}
+
+}

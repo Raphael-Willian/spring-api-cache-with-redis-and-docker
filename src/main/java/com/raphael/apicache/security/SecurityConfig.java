1974fc5266a4beafa1ec4f0f0e4529b8059e5cfb
Raphael Willian <raphaelwillian.cardososilva@gmail.com>
2026-03-31 08:42:56 -0300

    feat: Adicionado Política de segurança STATELESS no filterchain

--- src/main/java/com/raphael/apicache/security/SecurityConfig.java
+++ src/main/java/com/raphael/apicache/security/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -21,9 +22,10 @@ public class SecurityConfig {
                         .requestMatchers("/api/v1/produto").permitAll() //Rotas que todas as requests estarão permitidas (OBS: mudará para rotas de LOGIN quando a modelagem de dados for feita)
                         .anyRequest().authenticated() // As demais Rotas Exigem Autenticação
                 )
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .httpBasic(Customizer.withDefaults())
                 .build();
 
-    }
+    } //git push -u origin <nome-da-branch> Executar no proximo push, para que o git entenda que os pushs serão default pra essa branch no github
 
 }
